c13d185f5c3b232503ca7b7512ebfd52178a4050
Ashutosh Shelke <ashutoshshelke.snj@gmail.com>
2025-09-13 23:47:01 +0530

    13-9 challange solve

---
+++ Daily_Question/Find_Most_Frequent_Vowel_and_Consonant.java
@@ -0,0 +1,27 @@
+package Daily_Question;
+
+public class Find_Most_Frequent_Vowel_and_Consonant {
+    public int maxFreqSum(String s) {
+        int[] freq = new int[26];
+        for (int i = 0; i < s.length(); i++) {
+            freq[s.charAt(i) - 'a']++;
+        }
+
+        int maxVowel = 0;
+        int maxConsonant = 0;
+
+        for (int i = 0; i < 26; i++) {
+            char ch = (char) ('a' + i);
+            if (isVowel(ch)) {
+                maxVowel = Math.max(maxVowel, freq[i]);
+            } else {
+                maxConsonant = Math.max(maxConsonant, freq[i]);
+            }
+        }
+
+        return maxVowel + maxConsonant;
+    }
+    private boolean isVowel(char ch) {
+        return (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u');
+    }
+}
