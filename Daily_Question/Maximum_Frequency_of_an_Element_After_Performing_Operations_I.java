2b746081bd50cb081ccca1e234e0eb78643a78e0
Ashutosh Shelke <ashutoshshelke.snj@gmail.com>
2025-10-21 22:26:08 +0530

    21-10 Challanhe Solve

---
+++ Daily_Question/Maximum_Frequency_of_an_Element_After_Performing_Operations_I.java
@@ -0,0 +1,35 @@
+package Daily_Question;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.TreeMap;
+import java.util.TreeSet;
+
+public class Maximum_Frequency_of_an_Element_After_Performing_Operations_I {
+
+	  public int maxFrequency(int[] nums, int k, int numOperations) {
+	    int ans = 1;
+	    int adjustable = 0;
+	    Map<Integer, Integer> count = new HashMap();
+	    TreeMap<Integer, Integer> line = new TreeMap();
+	    TreeSet<Integer> candidates = new TreeSet();
+
+	    for (final int num : nums) {
+	      count.merge(num, 1, Integer::sum);
+	      line.merge(num - k, 1, Integer::sum);
+	      line.merge(num + k + 1, -1, Integer::sum);
+	      candidates.add(num);
+	      candidates.add(num - k);
+	      candidates.add(num + k + 1);
+	    }
+
+	    for (final int num : candidates) {
+	      adjustable += line.getOrDefault(num, 0);
+	      final int countNum = count.getOrDefault(num, 0);
+	      final int adjusted = adjustable - countNum;
+	      ans = Math.max(ans, countNum + Math.min(numOperations, adjusted));
+	    }
+
+	    return ans;
+	  }
+	}
