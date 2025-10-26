2db216ce4e4c830049876cf443db2dd6d41ad202
Ashutosh Shelke <ashutoshshelke.snj@gmail.com>
2025-10-26 21:59:33 +0530

    26-10 Challange Solve

---
+++ Daily_Question/Simple_Bank_System.java
@@ -0,0 +1,35 @@
+package Daily_Question;
+
+public class Simple_Bank_System {
+
+    private long[] balance;
+
+    public  Simple_Bank_System (long[] balance) {
+        this.balance = balance;
+    }
+
+    private boolean isValidAccount(int account) {
+        return account >= 1 && account <= balance.length;
+    }
+
+    public boolean transfer(int account1, int account2, long money) {
+        if (!isValidAccount(account1) || !isValidAccount(account2)) return false;
+        if (balance[account1 - 1] < money) return false;
+        balance[account1 - 1] -= money;
+        balance[account2 - 1] += money;
+        return true;
+    }
+
+    public boolean deposit(int account, long money) {
+        if (!isValidAccount(account)) return false;
+        balance[account - 1] += money;
+        return true;
+    }
+
+    public boolean withdraw(int account, long money) {
+        if (!isValidAccount(account)) return false;
+        if (balance[account - 1] < money) return false;
+        balance[account - 1] -= money;
+        return true;
+    }
+}
