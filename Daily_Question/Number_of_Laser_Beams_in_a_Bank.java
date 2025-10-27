a4d30f2c39a394d65290e1a9416100f9afa8fb6d
Ashutosh Shelke <ashutoshshelke.snj@gmail.com>
2025-10-27 23:04:25 +0530

    27-10 Challange SOlve

---
+++ Daily_Question/Number_of_Laser_Beams_in_a_Bank.java
@@ -0,0 +1,18 @@
+package Daily_Question;
+
+public class Number_of_Laser_Beams_in_a_Bank {
+	  public int numberOfBeams(String[] bank) {
+	    int ans = 0;
+	    int prevOnes = 0;
+
+	    for (final String row : bank) {
+	      final int ones = (int) row.chars().filter(c -> c == '1').count();
+	      if (ones > 0) {
+	        ans += prevOnes * ones;
+	        prevOnes = ones;
+	      }
+	    }
+
+	    return ans;
+	  }
+	}
