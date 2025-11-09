e08aa62d365cc979e4a46be0ad7ba77cba3ce5fc
Ashutosh Shelke <ashutoshshelke.snj@gmail.com>
2025-11-09 22:24:23 +0530

    9-11 Challange Solve

--- Daily_Question/Minimum_One_Bit_Operations_to_Make_Integers_Zero.java
+++ Daily_Question/Minimum_One_Bit_Operations_to_Make_Integers_Zero.java
@@ -1,5 +1,13 @@
 package Daily_Question;
 
 public class Minimum_One_Bit_Operations_to_Make_Integers_Zero {
-
-}
+	  public int minimumOneBitOperations(int n) {
+	    if (n == 0)
+	      return 0;
+	   
+	    int x = 1;
+	    while (x * 2 <= n)
+	      x <<= 1;
+	    return minimumOneBitOperations(n ^ (x | x >> 1)) + 1 + x - 1;
+	  }
+	}
