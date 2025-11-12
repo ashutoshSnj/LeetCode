813ae47f5036e91bc9f487b3aab9ad2fe0a71319
Ashutosh Shelke <ashutoshshelke.snj@gmail.com>
2025-11-12 22:37:52 +0530

    12-11 Challange Solve

---
+++ Daily_Question/Minimum_Number_of_Operations_to_Make_All_Array_Elements_Equal_to_1.java
@@ -0,0 +1,29 @@
+package Daily_Question;
+
+import java.util.Arrays;
+
+public class Minimum_Number_of_Operations_to_Make_All_Array_Elements_Equal_to_1 {
+	  public int minOperations(int[] nums) {
+	    final int n = nums.length;
+	    final int ones = (int) Arrays.stream(nums).filter(num -> num == 1).count();
+	    if (ones > 0)
+	      return n - ones;
+	    int minOps = Integer.MAX_VALUE;
+
+	    for (int i = 0; i < n; ++i) {
+	      int g = nums[i];
+	      for (int j = i + 1; j < n; ++j) {
+	        g = gcd(g, nums[j]);
+	        if (g == 1) { 
+	          minOps = Math.min(minOps, j - i);
+	          break;
+	        }
+	      }
+	    }
+	    return minOps == Integer.MAX_VALUE ? -1 : minOps + n - 1;
+	  }
+
+	  private int gcd(int a, int b) {
+	    return b == 0 ? a : gcd(b, a % b);
+	  }
+	}
