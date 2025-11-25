e6888b0bb6162b4b86da66018ad36e3867670eb0
Ashutosh Shelke <ashutoshshelke.snj@gmail.com>
2025-11-25 21:50:50 +0530

    25-11 CHallange SOlve

---
+++ Daily_Question/Smallest_Integer_Divisible_by_K.java
@@ -0,0 +1,25 @@
+package Daily_Question;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class Smallest_Integer_Divisible_by_K {
+	  public int smallestRepunitDivByK(int k) {
+	    if (k % 10 != 1 && k % 10 != 3 && k % 10 != 7 && k % 10 != 9)
+	      return -1;
+
+	    Set<Integer> seen = new HashSet<>();
+	    int n = 0;
+
+	    for (int length = 1; length <= k; ++length) {
+	      n = (n * 10 + 1) % k;
+	      if (n == 0)
+	        return length;
+	      if (seen.contains(n))
+	        return -1;
+	      seen.add(n);
+	    }
+
+	    return -1;
+	  }
+	}
