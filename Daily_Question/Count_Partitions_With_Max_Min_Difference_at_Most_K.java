752303cfd78453e4940d17cfdc946a8657032dc2
Ashutosh Shelke <ashutoshshelke.snj@gmail.com>
2025-12-06 22:39:51 +0530

    06-12 Challange Solve

---
+++ Daily_Question/Count_Partitions_With_Max_Min_Difference_at_Most_K.java
@@ -0,0 +1,48 @@
+package Daily_Question;
+
+import java.util.TreeMap;
+
+public class Count_Partitions_With_Max_Min_Difference_at_Most_K {
+
+    public int countPartitions(int[] nums, int k) {
+        final int MOD = (int) 1e9 + 7;
+      
+        TreeMap<Integer, Integer> windowElements = new TreeMap<>();
+      
+        int n = nums.length;
+      
+        
+        int[] dp = new int[n + 1];
+        int[] prefixSum = new int[n + 1];
+        dp[0] = 1;
+        prefixSum[0] = 1;
+        int left = 1;
+      
+       
+        for (int right = 1; right <= n; right++) {
+            int currentNum = nums[right - 1];
+          
+            windowElements.merge(currentNum, 1, Integer::sum);
+          
+            // Shrink window from left while range exceeds k
+            while (windowElements.lastKey() - windowElements.firstKey() > k) {
+                int leftNum = nums[left - 1];
+              
+                // Remove leftmost element from window
+                if (windowElements.merge(leftNum, -1, Integer::sum) == 0) {
+                    windowElements.remove(leftNum);
+                }
+                left++;
+            }
+          
+           
+            int previousSum = (left >= 2) ? prefixSum[left - 2] : 0;
+            dp[right] = (prefixSum[right - 1] - previousSum + MOD) % MOD;
+          
+            // Update prefix sum
+            prefixSum[right] = (prefixSum[right - 1] + dp[right]) % MOD;
+        }
+      
+        return dp[n];
+    }
+}
--- Daily_Question/Count_Partitions_with_Even_Sum_Difference.java
+++ Daily_Question/Count_Partitions_with_Even_Sum_Difference.java
@@ -4,7 +4,6 @@ import java.util.Arrays;
 
 public class Count_Partitions_with_Even_Sum_Difference {
 	  public int countPartitions(int[] nums) {
-	  
 	    return Arrays.stream(nums).sum() % 2 == 0 ? nums.length - 1 : 0;
 	  }
 	}
