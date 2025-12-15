250ff18313eea52b1b723731532fa68edf5b9ce9
Ashutosh Shelke <ashutoshshelke.snj@gmail.com>
2025-12-15 22:59:44 +0530

    15-12 CHallange Solve

---
+++ Daily_Question/Number_of_Smooth_Descent_Periods_of_a_Stock.java
@@ -0,0 +1,18 @@
+package Daily_Question;
+
+public class Number_of_Smooth_Descent_Periods_of_a_Stock {
+	  public long getDescentPeriods(int[] prices) {
+	    long ans = 1;
+	    int dp = 1;
+
+	    for (int i = 1; i < prices.length; ++i) {
+	      if (prices[i] == prices[i - 1] - 1)
+	        ++dp;
+	      else
+	        dp = 1;
+	      ans += dp;
+	    }
+
+	    return ans;
+	  }
+	}
