4625816c1c7402b2b040777952dc458455be8bfa
Ashutosh Shelke <ashutoshshelke.snj@gmail.com>
2025-12-24 22:11:55 +0530

    Apple Redistribution into Boxes

---
+++ Daily_Question/Apple_Redistribution_into_Boxes.java
@@ -0,0 +1,20 @@
+package Daily_Question;
+
+import java.util.Arrays;
+
+public class Apple_Redistribution_into_Boxes {
+	  public int minimumBoxes(int[] apple, int[] capacity) {
+	    final int appleSum = Arrays.stream(apple).sum();
+	    int capacitySum = 0;
+
+	    Arrays.sort(capacity);
+
+	    for (int i = 0; i < capacity.length; ++i) {
+	      capacitySum += capacity[capacity.length - 1 - i];
+	      if (capacitySum >= appleSum)
+	        return i + 1;
+	    }
+
+	    return capacity.length;
+	  }
+	}
