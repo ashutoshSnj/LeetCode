cc1f8306c34223ae17807a3d809391c54c238df0
Ashutosh Shelke <ashutoshshelke.snj@gmail.com>
2026-01-02 23:21:21 +0530

    N-Repeated Element in Size 2N Array

---
+++ Daily_Question/NRepeated_Element_in_Size_2N_Array.java
@@ -0,0 +1,10 @@
+package Daily_Question;
+
+public class NRepeated_Element_in_Size_2N_Array {
+	  public int repeatedNTimes(int[] nums) {
+	    for (int i = 0; i + 2 < nums.length; ++i)
+	      if (nums[i] == nums[i + 1] || nums[i] == nums[i + 2])
+	        return nums[i];
+	    return nums[nums.length - 1];
+	  }
+}
